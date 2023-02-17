d80392f88b4af6c874eb4a97aefd8c9866cf36fb
Evan Stoll <evanjsx@gmail.com>
2023-02-17 11:20:33 -0500

    Model: make all Model fields optional except id, name, type_field

--- src/model/model.rs
+++ src/model/model.rs
@@ -10,24 +10,24 @@ use crate::model::model_version::ModelVersion;
 pub struct Model {
     pub id: i64,
     pub name: String,
-    pub description: String,
+    pub description: Option<String>,
     #[serde(rename = "type")]
     pub type_field: String,
-    pub poi: bool,
-    pub nsfw: bool,
-    pub allow_no_credit: bool,
-    pub allow_commercial_use: String,
-    pub allow_derivatives: bool,
-    pub allow_different_license: bool,
-    pub creator: Creator,
-    pub tags: Vec<String>,
+    pub poi: Option<bool>,
+    pub nsfw: Option<bool>,
+    pub allow_no_credit: Option<bool>,
+    pub allow_commercial_use: Option<String>,
+    pub allow_derivatives: Option<bool>,
+    pub allow_different_license: Option<bool>,
+    pub creator: Option<Creator>,
+    pub tags: Option<Vec<String>>,
     pub model_versions: Vec<ModelVersion>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Creator {
-    pub username: String,
+    pub username: Option<String>,
     pub image: Option<String>,
 }
 
